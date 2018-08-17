e75eef42abae14977955673df94c2e39ed72f9b9
Rashed Abdel-Tawab <rashed@linux.com>
2018-08-17 09:20:21 -0700

    build: Ignore neverallows
    Change-Id: I35a39ec73c0047f93124f72249a6fa936fe0875a

--- core/config.mk
+++ core/config.mk
@@ -1103,6 +1103,11 @@ $(eval include device/colt/sepolicy/common/sepolicy.mk)
 # Include any vendor specific apicheck.mk file
 -include $(TOPDIR)vendor/*/build/core/apicheck.mk
 
+# We modify several neverallows, so let the build proceed
+ifneq ($(TARGET_BUILD_VARIANT),user)
+SELINUX_IGNORE_NEVERALLOWS := true
+endif
+
 # Rules for QCOM targets
 -include $(TOPDIR)vendor/colt/build/core/qcom_target.mk
 
@@ -1111,3 +1116,4 @@ $(eval include device/colt/sepolicy/common/sepolicy.mk)
 endif
 
 include $(BUILD_SYSTEM)/dumpvar.mk
+
