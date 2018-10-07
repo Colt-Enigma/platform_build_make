dd1171c938dbb2d0b1e9284952b2ab37aac05f60
RakeshBatra <raakesh.batra@rediffmail.com>
2018-10-07 06:28:07 +0000

    ASCII and Completion msg updation
    Signed-off-by: RakeshBatra <raakesh.batra@rediffmail.com>

--- tools/releasetools/ota_from_target_files.py
+++ tools/releasetools/ota_from_target_files.py
@@ -820,6 +820,20 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
   # Dump fingerprints
   script.Print("Target: {}".format(target_info.fingerprint))
 
+  #Print ASCII
+  script.Print("**************************************************");
+  script.Print("* e88~-_           888   d8     ,88~-_   ,d88~~\ *");
+  script.Print("*d888   \  e88~-_  888 _d88__  d888   \  8888    *");
+  script.Print("*8888     d888   i 888  888   88888    | `Y88b   *");
+  script.Print("*8888     8888   | 888  888   88888    |  `Y88b, *");
+  script.Print("*Y888   / Y888   ' 888  888    Y888   /     8888 *");
+  script.Print("* '88_-~   '88_-~  888  '88_/   `88_-~   \__88P' *");
+  script.Print("**************************************************");
+  script.Print("                by Rakesh Batra                  *");
+  script.Print("             Have a nice experience              *");
+  script.Print("**************************************************");
+  script.Print(" ")
+  script.AppendExtra("sleep (2);")
   script.AppendExtra("ifelse(is_mounted(\"/system\"), unmount(\"/system\"));")
   device_specific.FullOTA_InstallBegin()
 
