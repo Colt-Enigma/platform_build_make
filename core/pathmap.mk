AospExtended/platform_build

core: Include Aosp pathmap
Change-Id: Ie25dee383cc5bc9bb6390cff2cb2460d526d80b6

core/pathmap.mk
@@ -93,3 +93,7 @@ FRAMEWORKS_BASE_SUBDIRS := \
 #
 FRAMEWORKS_BASE_JAVA_SRC_DIRS := \
 	$(addprefix frameworks/base/,$(FRAMEWORKS_BASE_SUBDIRS))
+
+ifneq ($(CUSTOM_BUILD),)
+-include $(TOPDIR)vendor/aosp/build/core/pathmap.mk
+endif

Colt-Enigma/platform_build_make

Signed-off-by: RakeshBatra <raakesh.batra@rediffmail.com>

core/pathmap.mk
@@ -93,3 +93,7 @@ FRAMEWORKS_BASE_SUBDIRS := \
 #
 FRAMEWORKS_BASE_JAVA_SRC_DIRS := \
 	$(addprefix frameworks/base/,$(FRAMEWORKS_BASE_SUBDIRS))
+
+ifneq ($(CUSTOM_BUILD),)
+-include $(TOPDIR)vendor/colt/build/core/pathmap.mk
+endif