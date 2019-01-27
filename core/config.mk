8fc23081596a263e5d1e4afe22c07989018fc18f
RakeshBatra <raakesh.batra@rediffmail.com>
2019-01-27 04:02:11 +0000

    Revert "don't build with sepolicy as of now"
    This reverts commit 1e8b338b804296c1c221d0328b18c5fe9ff0c8dc.

--- core/config.mk
+++ core/config.mk
@@ -1086,7 +1086,7 @@ endif
 ifneq ($(CUSTOM_BUILD),)
 ## We need to be sure the global selinux policies are included
 ## last, to avoid accidental resetting by device configs
-#$(eval include device/colt/sepolicy/common/sepolicy.mk)
+$(eval include device/colt/sepolicy/common/sepolicy.mk)
 
 # Include any vendor specific config.mk file
 -include $(TOPDIR)vendor/*/build/core/config.mk
