15a120965135c179be76f58437d58791f443df2e
Nikesh001 <nikkataria74@gmail.com>
2019-12-11 17:29:26 +0000

    build: remove some unwanted packages

--- target/product/mainline.mk
+++ target/product/mainline.mk
@@ -38,8 +38,7 @@ $(call inherit-product, frameworks/base/data/sounds/AudioPackage14.mk)
 endif
 
 PRODUCT_PACKAGES += \
-    PhotoTable \
-    WallpaperPicker \
+    PhotoTable
 
 ifeq ($(COLT_BUILD),)
 PRODUCT_COPY_FILES += device/sample/etc/apns-full-conf.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/apns-conf.xml
