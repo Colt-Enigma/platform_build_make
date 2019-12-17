78954a2284bfd929035cadd64db04df0f66c8063
RakeshBatra <raakesh.batra@rediffmail.com>
2019-12-17 03:19:51 +0000

    Complete Flashing Script

--- tools/releasetools/ota_from_target_files.py
+++ tools/releasetools/ota_from_target_files.py
@@ -962,6 +962,9 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
     # Stage 3/3: Make changes.
     script.Comment("Stage 3/3")
 
+  # Inform User
+  script.Print("BackupTool at Work");
+
   # Dump fingerprints
   script.Print("Target: {}".format(target_info.fingerprint))
 
@@ -978,7 +981,46 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
     script.RunBackup("backup")
     script.Unmount("/system")
 
+  script.Print("BackupTool Work Done");
+
   system_progress = 0.75
+
+  script.Print("************************************");
+  script.Print("*     ___      _ _       ___  __   *");
+  script.Print("*    / __\___ | | |_    /___\/ _\  *");
+  script.Print("*   / /  / _ \| | __|  //  //\ \   *");
+  script.Print("*  / /__| (_) | | |_  / \_// _\ \  *");
+  script.Print("*  \____/\___/|_|\__| \___/  \__/  *");
+  script.Print("*                                  *");
+  script.Print("************************************");
+  script.Print("*    by Rakesh Batra (*TeamColt)   *");
+  script.Print("*       Its Android 10 Enjoy       *");
+  script.Print("************************************");
+
+  coltversion = target_info.GetBuildProp("ro.colt.display.version")
+  androidver = target_info.GetBuildProp("ro.build.version.release")
+  buildtype = target_info.GetBuildProp("ro.build.type")
+  buildid = target_info.GetBuildProp("ro.build.id")
+  buildday = target_info.GetBuildProp("ro.build.date")
+  securep = target_info.GetBuildProp("ro.build.version.security_patch")
+  device = target_info.GetBuildProp("ro.product.name")
+  manufacturer = target_info.GetBuildProp("ro.product.manufacturer")
+
+  script.Print("***********************************************");
+  script.Print(" ROM version      : %s"%(coltversion));
+  script.Print("");
+  script.Print(" Android version  : %s"%(androidver));
+  script.Print("");
+  script.Print(" Security patch   : %s"%(securep));
+  script.Print("");
+  script.Print(" Build date       : %s"%(buildday));
+  script.Print("");
+  script.Print(" Build type       : %s"%(buildtype));
+  script.Print("");
+  script.Print(" Device           : %s"%(device));
+  script.Print("");
+  script.Print(" Manufacturer     : %s"%(manufacturer));
+  script.Print("***********************************************");
 
   if OPTIONS.wipe_user_data:
     system_progress -= 0.1
@@ -1046,6 +1088,8 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
 
   script.ShowProgress(0.2, 10)
   device_specific.FullOTA_InstallEnd()
+
+  script.Print("Welcome to ColtOS : A Custom ROM by TeamColt");
 
   if OPTIONS.extra_script is not None:
     script.AppendExtra(OPTIONS.extra_script)
