b6b643736b73a0bf7e73d79475c68d4735b60665
RakeshBatra <raakesh.batra@rediffmail.com>
2021-11-04 11:18:14 -0700

    Add ColtOS Package Flashing Script
    Signed-off-by: RakeshBatra <raakesh.batra@rediffmail.com>

--- tools/releasetools/non_ab_ota.py
+++ tools/releasetools/non_ab_ota.py
@@ -211,6 +211,43 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
   # Dump fingerprints
   script.Print("Target: {}".format(target_info.fingerprint))
 
+  script.Print("=================================================");
+  script.Print("**           ___      _ _       ___  __        **");
+  script.Print("**          / __\___ | | |_    /___\/ _\       **");
+  script.Print("**         / /  / _ \| | __|  //  //\ \        **");
+  script.Print("**        / /__| (_) | | |_  / \_// _\ \       **");
+  script.Print("**        \____/\___/|_|\__| \___/  \__/       **");
+  script.Print("**                                             **");
+  script.Print("=================================================");
+  script.Print("**                  by TeamColt                **");
+  script.Print("**           Be Ready To Get Colt-ify          **");
+  script.Print("=================================================");
+
+  coltversion = target_info.GetBuildProp("ro.colt.build.version")
+  androidver = target_info.GetBuildProp("ro.build.version.release")
+  buildtype = target_info.GetBuildProp("ro.build.type")
+  buildid = target_info.GetBuildProp("ro.build.id")
+  buildday = target_info.GetBuildProp("ro.build.date")
+  securep = target_info.GetBuildProp("ro.build.version.security_patch")
+  device = target_info.GetBuildProp("ro.product.device")
+  manufacturer = target_info.GetBuildProp("ro.product.manufacturer")
+
+  script.Print("=================================================");
+  script.Print(" Colt version     : %s"%(coltversion));
+  script.Print("");
+  script.Print(" Android version  : %s"%(androidver));
+  script.Print("");
+  script.Print(" Security patch   : %s"%(securep));
+  script.Print("");
+  script.Print(" Build date       : %s"%(buildday));
+  script.Print("");
+  script.Print(" Build type       : %s"%(buildtype));
+  script.Print("");
+  script.Print(" Device           : %s"%(device));
+  script.Print("");
+  script.Print(" Manufacturer     : %s"%(manufacturer));
+  script.Print("=================================================");
+
   device_specific.FullOTA_InstallBegin()
 
   # All other partitions as well as the data wipe use 10% of the progress, and
